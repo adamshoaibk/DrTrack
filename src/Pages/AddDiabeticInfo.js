b3aa77c08fcb36cb9618f94779306e116af6fe84
adamshoaibk <adamshoaib.k@gmail.com>
2021-09-26 22:22:52 +0530

    initial setup, router, graph, icons

---
+++ src/Pages/AddDiabeticInfo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class AddDiabeticInfo extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>AddDiabeticInfo!</Text>
+      </View>
+    );
+  }
+}
+
+export default AddDiabeticInfo;
